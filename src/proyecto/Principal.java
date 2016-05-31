793bb400a91473756600bafb1fa16f9c6ec6e2ff
jsnaranjoh <jsnaranjoh@outlook.com>
2016-05-31 18:11:31 -0500

    25% de implementacion de leer entrada

--- src/proyecto/Principal.java
+++ src/proyecto/Principal.java
@@ -6,6 +6,9 @@
  */
 package proyecto;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.List;
 import lpsolve.LpSolve;
 import lpsolve.LpSolveException;
@@ -30,15 +33,30 @@ public class Principal {
     }
     
     public void leerEntrada() {
+        List<String> listaLineas = new ArrayList();
         
+        try {
+            FileReader fr = new FileReader("entrada.txt");
+            BufferedReader br = new BufferedReader(fr);
+            String linea;
+            while((linea = br.readLine()) != null) {
+                listaLineas.add(linea);
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        
+        String[] listaTokensPrimeraLinea = listaLineas.get(0).split("\\s");
+        Integer nAldeas = Integer.parseInt(listaTokensPrimeraLinea[0]);
+        Integer nAviones = Integer.parseInt(listaTokensPrimeraLinea[1]);
     }
     
     public void optimizar() {
+        alimentos = new Integer[0][0];
+        vuelos = new Integer[0][0];
+        
         try {
-            alimentos = new Integer[0][0];
-            vuelos = new Integer[0][0];
             lpS = LpSolve.makeLp(0, 0);
-            
         } catch (LpSolveException ex) {
             System.out.println(ex.getMessage());
         }
